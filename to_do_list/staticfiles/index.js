7fe660edb3f01fa47148f84c7d2856ecda3b6eed
Rajat Kumar <rajat02032002@gmail.com>
2025-11-23 12:09:04 +0530

    Deploment ISsue Resolution 4

--- to_do_list/staticfiles/index.js
+++ to_do_list/staticfiles/index.js
@@ -1,9 +1,9 @@
 let API_BASE;
 
 if (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1") {
-    API_BASE = "http://localhost:8000/";
+    API_BASE = "http://localhost:8000";
 } else {
-    API_BASE = "https://to-do-list-3-i9cn.onrender.com/";
+    API_BASE = "https://to-do-list-3-i9cn.onrender.com";
 }
 
 const app = document.getElementById('app');
